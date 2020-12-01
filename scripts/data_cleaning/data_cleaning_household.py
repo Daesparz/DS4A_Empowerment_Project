e607131ec1896a68c2201cb98c6cafc5ff3cfcad
Daesparz <daesparz@uc.cl>
2020-12-01 02:48:20 -0800

    cleaning methods

--- scripts/data_cleaning/data_cleaning_household.py
+++ scripts/data_cleaning/data_cleaning_household.py
@@ -2,13 +2,6 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 
-def load_combined_raw_data():
-    """NOTE: Modify if your data storage is different"""
-    data = [
-        pd.read_csv(f'../data/pulse2020_puf_{n:02d}.csv')
-        for n in WEEK_IDX_RANGE
-    ]
-    return pd.concat(data, ignore_index=True)
 
 def add_dates_start_end_inplace(data):
     data['DATE_START'] = data['WEEK'].apply(lambda idx: DATES_FROM_WEEK_IDX[idx][0])
@@ -20,28 +13,3 @@ def add_age_inplace(data):
 def replace_codes_with_nan_inplace(data):
     data.replace(-88, np.nan, inplace=True)
     data.replace(-99, np.nan, inplace=True)
-
-def get_col_list(col_name, num):
-    return [col_name + str(i) for i in range(1, num+1)]
-
-def drop_cols(dta):
-    cols_dict = {'WHEREFREE':7, 'FOODSUFRSN':5}
-    for c in list(cols_dict.keys()):
-        col_lst = get_col_list(c, cols_dict[c])
-        if c == 'FOODSUFRSN':
-            foodsuf_sum = dta[col_lst].sum(axis = 1).values
-            dta[(c+'_SUM')] = foodsuf_sum
-        dta.drop(col_lst, 1, inplace = True)
-    #return dta
-
-def get_cleaned_combined_data():
-    data = load_combined_raw_data()
-
-    replace_codes_with_nan_inplace(data)
-
-    add_dates_start_end_inplace(data)
-
-    add_age_inplace(data)
-    add_mental_vars_inplace(data)
-    drop_cols(data)
-    return data
